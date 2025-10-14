6598e00314e50bbe338b21bc0357c825221d3aff
HamdyAR <aremu.hamdalah@gmail.com>
2025-10-14 16:09:49 +0100

    fix(test): BookServiceTest - Fixed NullPointerException in getBookById() validation
    - Problem: testFindBookById() failed due to NullPointerException in BookService.getBookById()
    - Root Cause: bookMapper dependency was not injected and mocked in test, causing NullPointerException when mapping Book entity to BookDTO.
    - Solution: Added mock BookDTO, injected and mocked book mapper behaviour.
    - Impact: Ensures proper entity-to-DTO conversion and validates book retrieval logic.

--- backend/src/test/java/com/technicalchallenge/service/BookServiceTest.java
+++ backend/src/test/java/com/technicalchallenge/service/BookServiceTest.java
@@ -1,6 +1,7 @@
 package com.technicalchallenge.service;
 
 import com.technicalchallenge.dto.BookDTO;
+import com.technicalchallenge.mapper.BookMapper;
 import com.technicalchallenge.model.Book;
 import com.technicalchallenge.repository.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -18,17 +19,39 @@ import static org.mockito.Mockito.*;
 public class BookServiceTest {
     @Mock
     private BookRepository bookRepository;
+    @Mock
+    private BookMapper bookMapper;
     @InjectMocks
     private BookService bookService;
 
     @Test
     void testFindBookById() {
+        //Given
+        //creating Book mock object
         Book book = new Book();
         book.setId(1L);
+        book.setBookName("Book1");
+
+        //creating BookDTO mock object
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setId(1L);
+        bookDTO.setBookName("Book1");
+
+        //mocking bookRepository and bookMapper behaviour
         when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+        when(bookMapper.toDto(book)).thenReturn((bookDTO));
+        
+        //calling BookService method
         Optional<BookDTO> found = bookService.getBookById(1L);
+
+        //validating results
         assertTrue(found.isPresent());
         assertEquals(1L, found.get().getId());
+        assertEquals("Book1", found.get().getBookName());
+
+        //verifying mock interactions
+        verify(bookRepository).findById(1L);
+        verify(bookMapper).toDto(book);
     }
 
     @Test
